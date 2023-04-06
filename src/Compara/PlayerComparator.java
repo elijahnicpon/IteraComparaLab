adaec80e8c8a3dba4f2db34b383fe52f8e14233f
Elijah Nicpon <43706587+elijahnicpon@users.noreply.github.com>
2023-04-06 09:33:10 -0400

    Update PlayerComparator.java

--- src/Compara/PlayerComparator.java
+++ src/Compara/PlayerComparator.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 // TODO 1.7: Augment the class constructor to implement Comparator.
 public class PlayerComparator {
 
-    // TODO 1.8: Implement compareTo() to satisfy the Comparable interface. Make it so that the player with the larger damage is considered greater than the player with lesser damage.
+    // TODO 1.8: Implement compare() to satisfy the Comparable interface. Make it so that the player with the larger damage is considered greater than the player with lesser damage.
 
 
 }
